c13cf3d8ed26a4f89245abcaa5e5c404beac2524
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-28 13:05:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,9 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def key_for_min_value(birthday_hash)
 
 birthday_kids.collect do |kids_name, age|
-    age
+    
 
 end
