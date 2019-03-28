6ded8bd4000d6c36d9d037962d15a8ee59dbaec6
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-28 13:07:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,7 @@
 def key_for_min_value(birthday_hash)
 
 birthday_kids.collect do |kids_name, age|
-    
-
+  if age #how to sort age as a min_value
+  puts "The youngest person in the room is #{age} years old"
+  end
 end
