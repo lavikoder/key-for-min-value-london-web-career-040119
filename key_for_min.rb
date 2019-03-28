48d65a4865b0e4ce2b956e582dae0905f1625c6b
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-28 13:23:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(birthday_hash)
-  min_kid = 0
-  min_age = 0
+  min_kid = nil
+  min_age = nil
 
 #using each will allow you to return the single value
 
   birthday_hash.each do |kids_name, age|
-    if min_age == 0 || age < min_age 
+    if min_age == nil || age < min_age 
       
 #how to sort age as a min_value
       
