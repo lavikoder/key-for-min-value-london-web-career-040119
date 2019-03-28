48f083b939cf0eee5f7e398c13f2c41fb30ba95e
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-28 13:16:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -15,14 +15,3 @@ def key_for_min_value(birthday_hash)
     end
 end
 
-min_num = nil
-  min_key = nil
-
-  name_hash.each do |key, num|
-    if min_num == nil || num < min_num
-      min_key = key
-      min_num = num
-    end
-end
-  min_key
-end
